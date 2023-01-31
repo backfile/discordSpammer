db6aedd00982f207c326375b3872c3038f107ca7
Franco Caraffa <francocaraffa2005@gmail.com>
2023-01-30 23:40:04 -0300

    discordSpammer

---
+++ discordSpam.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+print(" ║█║▌║█║▌│║▌║▌█║ Created By Frann#8446  ║█║▌║█║▌│║▌║▌█║")
+
+
+token = input("Ingrese su token: ")
+channel = int(input("ID of channel: "))
+message = input("Message to spam: ")
+delay = int(input("Delay: "))
+
+def sendMessages(token, channel, message, delay):
+    data = {"content": message}
+    header = {"authorization": token}
+    url = 'https://discord.com/api/v9/channels/{}/messages'.format(channel)
+
+    while True:
+            r = requests.post(url, data=data, headers=header)
+            print(r.status_code)
+            if r.status_code == 200:
+                time.sleep(delay)
+            else:
+                print("Something went wrong")
+                break
+
+if __name__ == "__main__":
+    sendMessages(token, channel, message, delay)
